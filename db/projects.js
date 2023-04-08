0e7e591d45cd0c5a8e207868dba10902e612697b
an4morph <avgustin456@gmail.com>
2023-04-08 18:45:57 +0600

    add generators

--- db/projects.js
+++ db/projects.js
@@ -1,66 +1,92 @@
 const shortid = require('shortid')
+const { 
+  rand,
+  generateColor,
+  generateProjectName,
+  generateText,
+  getRandomImageLink
+} = require('../utils/utils.js')
 
-const project = {
-  id: shortid.generate(),
-  name: 'iShop',
-  blocks: [
-    {
-      id: shortid.generate(),
-      name: 'hero',
-      data: {
-        title: {
-          content: 'Welcome to our shop',
-          color: '#266e9a',
-          size: 48
+const generateFeaturesBlock = () => {
+  const cr = (Math.random() > 0.5) ? ['light', 'dark'] : ['dark', 'light']
+  return {
+    id: shortid.generate(),
+    name: 'features',
+    data: {
+      bgColor: generateColor(cr[0]),
+      titleColor: generateColor(cr[1]),
+      textColor: generateColor(cr[1]),
+      items: [
+        {
+          id: shortid.generate(),
+          title: generateText(5),
+          text: generateText(10),
+          image: getRandomImageLink(),
         },
-        text: {
-          content: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna.',
-          color: null,
-          size: 18
+        {
+          id: shortid.generate(),
+          title: generateText(5),
+          text: generateText(10),
+          image: getRandomImageLink(),
         },
-        bgColor: '#95daee',
-        image: 'https://plus.unsplash.com/premium_photo-1680497811614-4f93025d7e57?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80',
-        variants: 1,
-      }
-    },
-    {
-      id: shortid.generate(),
-      name: 'features',
-      data: {
-        bgColor: '#eee',
-        titleColor: null,
-        textColor: '#505050',
-        items: [
-          {
-            id: shortid.generate(),
-            title: 'Item 1',
-            text: 'Lorem ipsum dolor sit amet consectetur',
-          },
-          {
-            id: shortid.generate(),
-            title: 'Item 2',
-            text: 'Lorem ipsum dolor sit amet consectetur',
-          },
-          {
-            id: shortid.generate(),
-            title: 'Item 3',
-            text: 'Lorem ipsum dolor sit amet consectetur',
-          },
-          {
-            id: shortid.generate(),
-            title: 'Item 4',
-            text: 'Lorem ipsum dolor sit amet consectetur',
-          },
-        ]
-      }
+        {
+          id: shortid.generate(),
+          title: generateText(5),
+          text: generateText(10),
+          image: getRandomImageLink(),
+        },
+        {
+          id: shortid.generate(),
+          title: generateText(5),
+          text: generateText(10),
+          image: getRandomImageLink(),
+        },
+      ]
     }
-  ]
+  }
 }
 
+const generateHeroBlock = () => {
+  const cr = (Math.random() > 0.5) ? ['light', 'dark'] : ['dark', 'light']
+  return {
+    id: shortid.generate(),
+    name: 'hero',
+    data: {
+      title: {
+        content: generateText(10),
+        color: generateColor('bright'),
+        size: rand(26, 55)
+      },
+      text: {
+        content: generateText(50),
+        color: generateColor(cr[0]),
+        size: rand(14, 20)
+      },
+      bgColor: generateColor(cr[1]),
+      image: getRandomImageLink(),
+      variant: 1,
+    }
+  }
+}
+
+const genProject = (createdAt) => {
+  return {
+    id: shortid.generate(),
+    name: generateProjectName(),
+    createdAt,
+    blocks: [generateHeroBlock(), generateFeaturesBlock(), generateHeroBlock(), generateFeaturesBlock()]
+  }
+}
+
+const list = [
+  genProject('Sat, 08 Apr 2023 12:10:16 GMT'),
+  genProject('Sat, 08 Apr 2023 06:10:34 GMT'),
+  genProject('Fri, 07 Apr 2023 08:00:00 GMT'),
+]
+
 module.exports = {
-  main: [{
-    id: project.id,
-    name: project.name,
-  }],
-  expanded: [project]
+  main: list.map(({id, name, createdAt}) => ({
+    id, name, createdAt
+  })),
+  expanded: list
 }
--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,7 @@
+const loremIpsum = require('lorem-ipsum').loremIpsum
+
 const rand = (min, max) => Math.round(Math.random() * (max - min) + min)
-const genNumArr = (length, fromNum = 1) => Array(length).fill(0).map((_, index) => index + fromNum) 
+const generateNumArr = (length, fromNum = 1) => Array(length).fill(0).map((_, index) => index + fromNum) 
 
 const error = (res, status, text) => res.status(status).json(text).end()
 
@@ -9,9 +11,68 @@ const errorTexts = {
   type: (attr, t) => `${attr} attribute should be type "${t}"`,
 }
 
+function generateColor(tone = 'bright') {
+  const tones = {
+    dark: {
+      hue: Math.floor(Math.random() * 360),
+      saturation: Math.floor(Math.random() * 51) + 50,
+      lightness: Math.floor(Math.random() * 26),
+    },
+    light: {
+      hue: Math.floor(Math.random() * 360),
+      saturation: Math.floor(Math.random() * 51) + 50,
+      lightness: Math.floor(Math.random() * 26) + 75,
+    },
+    pastel: {
+      hue: Math.floor(Math.random() * 360),
+      saturation: Math.floor(Math.random() * 16) + 70,
+      lightness: Math.floor(Math.random() * 16) + 70,
+    },
+    bright: {
+      hue: Math.floor(Math.random() * 360),
+      saturation: Math.floor(Math.random() * 51) + 50,
+      lightness: Math.floor(Math.random() * 26) + 50,
+    },
+  };
+  const t = tones[tone] || tones.light; // use light tone as default
+  const h = t.hue;
+  const s = t.saturation;
+  const l = t.lightness;
+  return `hsl(${h},${s}%,${l}%)`;
+}
+
+
+const generateProjectName = () => {
+  const adjectives = ['fast', 'smart', 'modern', 'efficient', 'innovative', 'dynamic', 'progressive', 'perspective'];
+  const nouns = ['project', 'app', 'tool', 'solution', 'system', 'kit', 'hub', 'network'];
+
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  
+  return `${adjective} ${noun}`;
+}
+
+const generateText = (maxWords = 10) => {
+  const options = {
+    units: 'words',
+    count: Math.floor(Math.random() * maxWords) + 2, // генерируем от 2 до 11 слов
+    format: 'plain',
+  }
+  return loremIpsum(options);
+}
+
+const getRandomImageLink = (width = 800, height = 600) => {
+  return `https://source.unsplash.com/random/${width}x${height}`
+}
+
 module.exports = {
   rand,
-  genNumArr,
+  generateNumArr,
+  generateColor,
+  generateProjectName,
+  generateText,
+  getRandomImageLink,
+
   error,
   isObject,
   errorTexts
